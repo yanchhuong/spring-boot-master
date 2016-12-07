d0273626ab2c32edc3aacbc93ff22bfc1919851b
yanchhuong <yanchhuo_ng@yahoo.com>
2016-12-07 10:53:19 +0900

    sample

--- src/main/java/com/heroku/dao/impl/UsersDaoImpl.java
+++ src/main/java/com/heroku/dao/impl/UsersDaoImpl.java
@@ -1,5 +1,9 @@
 package com.heroku.dao.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import javax.annotation.PostConstruct;
 
 import org.apache.tomcat.jdbc.pool.DataSource;
@@ -42,6 +46,18 @@ public class UsersDaoImpl extends JdbcDaoSupport implements UsersDao{
         this.getJdbcTemplate().update(sql, new Object[]{user.getRegisterDate(),user.getFirst(),user.getLast()
         		,user.getUsername(),user.getEmail(),user.getUserCd()});
 		
+	}
+	@Override
+	public List<String> listUsername(List<String> username) {
+		 String sql = "SELECT fname FROM USER_DETAIL";
+		    List< Map < String, Object>> rows = this.getJdbcTemplate().queryForList(sql);
+		    String list;
+		    List<String> result = new ArrayList<String>();
+		    for(Map <String, Object> row:rows){
+		    	list=(String)row.get("fname");
+		        result.add(list);
+		    }   
+		    return result;
 	}
 
 }
